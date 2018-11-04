b6814e8c236a92af0c080f683d0e8e13e322ec34
Anatoliy <anatoliynbulyga@gmail.com>
2018-11-04 21:42:49 +0200

    audio ready

--- docs/js/audio.js
+++ docs/js/audio.js
@@ -1,5 +1,43 @@
 $(document).ready(function(){
 
+   var audio = $("#audioTrack")[0];
+
+   var playBtn = $("#audio-play"),
+      pauseBtn = $("#audio-pause"),
+      audioControl = $("[data-type='audio-control']");
+
+   setTimeout(initAudioPlayer, 1000);
+
+
+      
+   function initAudioPlayer() {
+      
+      playBtn.parent().show();
+      audio.loop = true;
+      audio.play();
+      playBtn.parent().hide();
+      pauseBtn.parent().show();
+
+
+      audioControl.click(function(){
+         
+         if ( audio.paused ) {
+
+            audio.play();
+            playBtn.parent().hide();
+            pauseBtn.parent().show();
+
+         } else {
+
+            audio.pause();
+            pauseBtn.parent().hide();
+            playBtn.parent().show();
+         }
+
+      });
+   }
+
+   // audio.play();
 
 
 });
